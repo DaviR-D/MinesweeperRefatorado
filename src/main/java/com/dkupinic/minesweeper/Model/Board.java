f5d1e9f3f2c0ede85d2ac258247621c400c3fde2
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-21 23:00:42 +0100

    deleted some code

--- src/main/java/com/dkupinic/minesweeper/Model/Board.java
+++ src/main/java/com/dkupinic/minesweeper/Model/Board.java
@@ -1,10 +1,5 @@
 package com.dkupinic.minesweeper.Model;
 
 public class Board {
-    private final int width = 16;
-    private final int height = 16;
-    public Board() {
-
-    }
 
 }
--- src/main/java/com/dkupinic/minesweeper/Model/Field.java
+++ src/main/java/com/dkupinic/minesweeper/Model/Field.java
@@ -1,8 +1,5 @@
 package com.dkupinic.minesweeper.Model;
 
 public class Field {
-    private int x;
-    private int y;
-    private boolean containsBomb = false;
-    private boolean isVisible = true;
+
 }
