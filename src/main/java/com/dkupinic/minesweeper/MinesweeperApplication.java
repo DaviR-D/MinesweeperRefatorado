a0c896be6133de4504a3152c7beae09ffe379231
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-23 22:43:10 +0100

    overall layout for game

--- src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        scene.getStylesheets().addAll(Objects.requireNonNull(getClass().getResource("styles/styles.css").toExternalForm()));
         stage.setTitle("Minesweeper");
         stage.setResizable(false);
         stage.setScene(scene);
