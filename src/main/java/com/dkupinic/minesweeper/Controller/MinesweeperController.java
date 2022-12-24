58d5cfa525961aa3fe01fa9e851734f93eb1d0dd
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-24 14:47:24 +0100

    minor changes

--- src/main/java/com/dkupinic/minesweeper/Controller/MinesweeperController.java
+++ src/main/java/com/dkupinic/minesweeper/Controller/MinesweeperController.java
@@ -1,28 +1,23 @@
 package com.dkupinic.minesweeper.Controller;
 
-import com.dkupinic.minesweeper.MinesweeperApplication;
-import com.dkupinic.minesweeper.Model.Board.BoardSize;
-import com.dkupinic.minesweeper.Model.Difficulty.Difficulty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MinesweeperController {
     @FXML
     private AnchorPane mainAnchor;
 
+    public static Pane pane;
+
     @FXML
-    public void initialize() throws IOException {
-        Pane pane = new Pane();
+    public void initialize() {
+        pane = new Pane();
         pane.setPrefSize(400,400);
-        pane.setStyle("-fx-background-color: red");
+        pane.setStyle("-fx-background-color: grey");
         mainAnchor.getChildren().add(pane);
-        BoardSize boardSize = new BoardSize(Difficulty.BEGINNER);
+
     }
+
 
 }
--- src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
@@ -3,6 +3,7 @@ package com.dkupinic.minesweeper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         scene.getStylesheets().addAll(Objects.requireNonNull(getClass().getResource("styles/styles.css").toExternalForm()));
+        stage.getIcons().add(new Image("file:src/main/resources/com/dkupinic/minesweeper/img/icon.png"));
         stage.setTitle("Minesweeper");
         stage.setResizable(false);
         stage.setScene(scene);
