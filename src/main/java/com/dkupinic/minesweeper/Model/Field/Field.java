fe2fa0ae6a4d47b5314578bf86ac6f5605e5f89b
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-24 14:47:08 +0100

    added constructor

--- src/main/java/com/dkupinic/minesweeper/Model/Field/Field.java
+++ src/main/java/com/dkupinic/minesweeper/Model/Field/Field.java
@@ -1,8 +1,21 @@
 package com.dkupinic.minesweeper.Model.Field;
 
-public class Field {
+import javafx.scene.Node;
+
+public class Field extends Node {
     private int x;
     private int y;
+    private boolean containsBomb;
+
+    public Field(int x, int y, boolean containsBomb) {
+        this.x = x;
+        this.y = y;
+        this.containsBomb = containsBomb;
+    }
 
 
+    @Override
+    public Node getStyleableNode() {
+        return super.getStyleableNode();
+    }
 }
