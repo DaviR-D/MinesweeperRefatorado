7edaa9b5648029f593c7e1cea312e7a044a845e5
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-01-19 19:01:31 +0100

    revamped UI

--- src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/dkupinic/minesweeper/MinesweeperApplication.java
@@ -13,7 +13,7 @@ public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MinesweeperApplication.class.getResource("minesweeper-main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         scene.getStylesheets().addAll(Objects.requireNonNull(Objects.requireNonNull(getClass().getResource("styles/styles.css")).toExternalForm()));
         stage.getIcons().add(new Image("file:src/main/resources/com/dkupinic/minesweeper/img/icon.png"));
         stage.setTitle("Minesweeper");
