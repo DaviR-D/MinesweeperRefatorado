b06385d7db31b26a39ac9f0b13e58dc8ee1c5b78
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-01-20 19:48:47 +0100

    small adjustments

--- src/main/java/com/dkupinic/minesweeper/Model/Timer.java
+++ src/main/java/com/dkupinic/minesweeper/Model/Timer.java
@@ -3,17 +3,16 @@ package com.dkupinic.minesweeper.Model;
 import com.dkupinic.minesweeper.Controller.MinesweeperController;
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
-import javafx.scene.control.Label;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Timer {
     private float timePlayed = 0;
     private long lastFrame = -1;
+    public static boolean activeTimer;
 
     public void startTimer(MinesweeperController controller) {
+        activeTimer = true;
         AnimationTimer animationTimer = new AnimationTimer() {
             @Override
             public void handle(long l) {
@@ -21,7 +20,7 @@ public class Timer {
                     timePlayed += (new Date().getTime() - lastFrame) / 1000.0;
                 }
                 lastFrame = new Date().getTime();
-                Platform.runLater(() -> controller.timerLabel.setText(String.format("%.2f", timePlayed)));
+                Platform.runLater(() -> controller.timerLabel.setText(String.format("%.2fs", timePlayed)));
             }
         };
         animationTimer.start();
